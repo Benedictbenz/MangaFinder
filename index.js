41e8d70db854e51124a07d70c4d8b424bd872a97
Benz <ocampobenedict1356@gmail.com>
2025-08-09 19:24:11 +0800

    Update
    Added some css codes

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.static("public"));
 
 const API_URL = "https://api.jikan.moe/v4";
 
+//================ 
+// HOME QUERY
+//================
 app.get("/", async(req,res)=>{
     try{
         const result = await axios.get(API_URL+"/random/manga",);
@@ -28,11 +31,46 @@ app.get("/", async(req,res)=>{
     }
 });
 
+// ================ 
+// RANDOM QUERY
+// ================
+
+app.get("/random", async(req,res)=>{
+    try{
+        const result = await axios.get(API_URL+"/random/manga",);
+
+        let result2 = result.data.data;
+
+        const data = result.data.data.url;
+        const title = result.data.data.titles;
+        const image = result.data.data.images.jpg.image_url;
+        const synopsis = result2.synopsis;
+        res.render("random.ejs",{content: data,titles:title, coverImage: image,synopsis:synopsis});
+    } catch(error){
+        res.render("random.ejs", {content: error.response});
+    }
+});
+
+//================
+// SEARCH QUERY
+//================
+
+app.get("/random", async(req,res)=>{
+    try{
+        const result = await axios.get(API_URL+"/random/manga",);
+
+        let result2 = result.data.data;
+
+        const data = result.data.data.url;
+        const title = result.data.data.titles;
+        const image = result.data.data.images.jpg.image_url;
+        const synopsis = result2.synopsis;
+        res.render("random.ejs",{content: data,titles:title, coverImage: image,synopsis:synopsis});
+    } catch(error){
+        res.render("random.ejs", {content: error.response});
+    }
+});
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
-
-function random(){
-    
-}
